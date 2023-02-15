d813c7f20c9728d5e957a0e4945d5ffe5e7f4070
Gabriela <gaby.9.camargo@outlook.com>
2023-02-15 18:35:23 -0300

    adicionada a metrica metrics.counter

--- app.py
+++ app.py
@@ -5,7 +5,13 @@ app = Flask(__name__)
 metrics = PrometheusMetrics(app)
 
 @app.route('/renda-fixa')
+
+#Vou construir uma métrica
+@metrics.counter('efetivacao_renda_variavel',
+                 'Número de papeis de renda fixa efetivados',
+                 labels = {'tipo':'ACOES'})
+
 def renda_fixa():
-    return render_template("lista.html", título="Renda fixa")
+    return render_template("lista.html", titulo="Renda fixa")
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
